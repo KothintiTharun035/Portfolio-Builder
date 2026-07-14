2feb764bafb904e4549e1dc32bef31a824979b8a
Kothinti Tharun <kothintitharun@gmail.com>
2026-07-14 22:38:04 +0530

    Update frontend API URL for production

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // const API = axios.create({ baseURL: "http://localhost:5000/" });
-const API = axios.create({ baseURL: "http://localhost:5001/" });
+const API = axios.create({
+  baseURL: "https://portfolio-builder-ekbf.onrender.com",
+});
 
 
 API.interceptors.request.use((req) => {
