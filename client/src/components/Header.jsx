e5d3c220802ae74a9f18ffa6b000cae9a0e61301
Kothinti Tharun <kothintitharun@gmail.com>
2026-07-15 00:00:11 +0530

    Fix duplicate React import

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { fetchCurrentUser } from "../redux/currentReducer";
